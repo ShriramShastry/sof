7bc18f61673c7e735d3bbbabb107332aec003e5c
ShriramShastry <malladi.sastry@intel.com>
2021-02-22 18:31:35 +0530

    Audio: tools: Added Statemachine handler

--- tools/math_bench/common/const.h
+++ tools/math_bench/common/const.h
@@ -4,8 +4,8 @@
 //constexpr auto TEST_TYPE_S16 = 3;
 //constexpr auto TEST_TYPE_S24 = 4;
 //constexpr auto TEST_TYPE_S32 = 5;
-#define  TEST_TYPE_FLOAT 1
-#define  TEST_TYPE_S8    2
-#define  TEST_TYPE_S16   3
-#define  TEST_TYPE_S24   4
-#define  TEST_TYPE_S32   5
+#define  TEST_TYPE_FLOAT 0
+#define  TEST_TYPE_S8    1
+#define  TEST_TYPE_S16   2
+#define  TEST_TYPE_S24   3
+#define  TEST_TYPE_S32   4
